d21e7b303cbb4e9847feb3be06501bba8a086bd2
Philipp Arbeit <philipp.hartl@zetaworks.at>
2022-09-01 10:27:40 +0200

    mine changes, consumable item fixes

---
+++ jars/src/com/fs/starfarer/api/impl/campaign/econ/impl/IndEvo_ArtilleryStation.java
@@ -0,0 +1,8 @@
+package com.fs.starfarer.api.impl.campaign.econ.impl;
+
+public class IndEvo_ArtilleryStation extends BaseIndustry {
+    //how to build
+    //requires metal and machinery to run, when deficit, do not fire
+    //station has to be defeated before colonizing?
+
+}
