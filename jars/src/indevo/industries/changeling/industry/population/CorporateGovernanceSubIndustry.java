da61da06a7441d46865a3398d2aa4b795776eac0
Philipp <philipp.hartl@zetaworks.at>
2023-06-21 00:30:54 +0200

    Haplo music

--- jars/src/indevo/industries/changeling/industry/population/CorporateGovernanceSubIndustry.java
+++ jars/src/indevo/industries/changeling/industry/population/CorporateGovernanceSubIndustry.java
@@ -3,7 +3,7 @@ package indevo.industries.changeling.industry.population;
 import com.fs.starfarer.api.campaign.econ.Industry;
 import indevo.industries.changeling.industry.SubIndustry;
 
-public class CorporateGovernanceSubIndustry extends SubIndustry {
+public class CorporateGovernanceSubIndustry extends SubIndustry{
 
     public CorporateGovernanceSubIndustry(String id, String name, String imageName, String descriptionID) {
         super(id, imageName, name, descriptionID);
