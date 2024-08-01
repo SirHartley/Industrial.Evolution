3d817ea469dba5e8eefedecfef72379191f5fc45
Philipp <philipp.hartl@zetaworks.at>
2024-08-01 22:26:57 +0200

    hm icon

--- jars/src/indevo/dialogue/research/hullmods/CamouflageFieldEmitter.java
+++ jars/src/indevo/dialogue/research/hullmods/CamouflageFieldEmitter.java
@@ -7,6 +7,8 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.impl.campaign.BaseGenericPlugin;
 import com.fs.starfarer.api.impl.campaign.ids.Tags;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 
 import java.util.Map;
 
@@ -34,5 +36,13 @@ public class CamouflageFieldEmitter extends BaseHullMod {
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
         super.applyEffectsBeforeShipCreation(hullSize, stats, id);
+    }
+
+    @Override
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, ShipAPI.HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
+        super.addPostDescriptionSection(tooltip, hullSize, ship, width, isForModSpec);
+
+        tooltip.addPara("Renders the ship %s to enemy patrols.", 10f, Misc.getHighlightColor(), "unrecognizable");
+        tooltip.addPara("The field has a tendency to cause visual glitches when malfunctioning under heavy fire.", 3f);
     }
 }
